120fc9032d98bffeffd7a4c6a3b3912ff1307ae5
MicahJank <MicahJank@users.noreply.github.com>
2019-09-29 20:14:31 -0500

    got everythign set up inside of the index.js

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,8 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import allReducers from './reducers';
 import App from "./components/App";
 
+const enhancer = compose(applyMiddleware(thunk, logger));
+
+const store = createStore(allReducers, enhancer);
+
 ReactDOM.render(
-<App />
+<Provider store={store}>
+    <App />
+</Provider>
 , document.getElementById("root"));
