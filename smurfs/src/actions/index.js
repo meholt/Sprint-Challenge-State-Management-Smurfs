253e42e45ca7cbe12c7150bd9879e953814c01eb
MicahJank <MicahJank@users.noreply.github.com>
2019-09-29 21:20:43 -0500

    posting is working and you can add smurfs to the village now

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,18 +1,30 @@
 import axios from 'axios';
 
-export const ADD_SMURF = 'ADD_SMURF';
 export const FETCH_SMURFS_PENDING = 'FETCH_SMURFS_PENDING';
 export const FETCH_SMURFS_SUCCESS = 'FETCH_SMURFS_SUCCESS';
-export const FETCH_SMURFS_FAIL = 'FETCH_SMURFS_FAIL'; 
+export const FETCH_SMURFS_FAIL = 'FETCH_SMURFS_FAIL';
 
-export const addSmurf = smurf => {
+export const ADD_SMURF_PENDING = 'ADD_SMURF_PENDING';
+export const ADD_SMURF_SUCCESS = 'ADD_SMURF_SUCCESS';
+export const ADD_SMURF_FAIL = 'ADD_SMURF_FAIL';
 
-    return {
-        type: ADD_SMURF,
-        payload: {
 
-        }
-    }
+export const addSmurf = (smurfInfo) => dispatch => {
+    dispatch({ type: ADD_SMURF_PENDING });
+    axios.post('http://localhost:3333/smurfs', smurfInfo)
+        .then(res => {
+            console.log(res);
+            dispatch({
+                type: ADD_SMURF_SUCCESS,
+                payload: res.data
+            })
+        })
+        .catch(err => {
+            dispatch({
+                type: ADD_SMURF_FAIL,
+                payload: err
+            })
+        })
 }
 
 export const fetchSmurfs = () => dispatch => {
--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -1,10 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const SmurfForm = () => {
+import { connect } from 'react-redux';
+
+import { addSmurf } from '../actions';
+
+const SmurfForm = ( { addSmurf } ) => {
+
+    const [formText, setFormText] = useState({
+        name: '',
+        height: '',
+        age: ''
+    })
+
+    const handleNameChanges = e => {
+        setFormText({
+            ...formText,
+            name: e.target.value
+        })
+    }
+
+    const handleHeightChanges = e => {
+        setFormText({
+            ...formText,
+            height: e.target.value
+        })
+    }
+
+    const handleAgeChanges = e => {
+        setFormText({
+            ...formText,
+            age: e.target.value
+        })
+    }
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addSmurf(formText);
+    }
 
     return (
-        <div>SmurfForm</div>
+        <form onSubmit={handleSubmit}>
+            <input
+                type='text'
+                name={formText.name}
+                value={formText.name}
+                onChange={handleNameChanges}
+                placeholder='Enter the smurfs name...'
+             />
+            
+            <input
+                type='text'
+                name={formText.height}
+                value={formText.height}
+                onChange={handleHeightChanges}
+                placeholder='Enter the smurfs height...'
+             />
+            
+            <input
+                type='text'
+                name={formText.age}
+                value={formText.age}
+                onChange={handleAgeChanges}
+                placeholder='Enter the smurfs age...'
+             />
+
+             <button type='submit'>Add Smurf</button>
+        </form>
     );
 }
 
-export default SmurfForm;
+
+export default connect(null, { addSmurf })(SmurfForm);
